aa509f58841e924bb8c9eb12216543b0cfc8cee6
eurocat2k <gabor.zelenak@me.com>
2024-01-11 12:07:22 +0000

    traversing ok

--- public/js/app.js
+++ public/js/app.js
@@ -40,4 +40,74 @@ $(function () {
     //         console.log({ f });
     //     });
     // });
+
+    function EmptyList() {
+        $('ul#filemanager').children().each((i, c) => {
+            $(c).empty();
+            $(c).remove();
+        });
+    }
+
+    let LastUP;
+
+    function RenderList(records) {
+        // if (records[0].IsDirectory) {
+        //     LastUP = records[0].Up;
+        //     let dom = `<li class="bi bi-folder" style="list-style: none;" data-type="d" data-up=".." data-path="${records[0].Up}/..">..</li>`;
+        //     $('ul#filemanager').append(dom);
+        // } else {
+        //     let dom = `<li class="bi bi-folder" style="list-style: none;" data-type="d" data-up=".." data-path="${LastUP}/..">..</li>`;
+        //     $('ul#filemanager').append(dom);
+        // }
+        let dom = `<li class="bi bi-folder" style="list-style: none;" data-type="d" data-up=".." data-path="${records[0].Up}">..</li>`;
+        $('ul#filemanager').append(dom);
+        for (let r in records) {
+            let entry = records[r];
+            // console.log({ r, entry }, entry.name, entry.type);
+            if (entry.IsDirectory) {
+                dom = `<li class="bi bi-folder" style="list-style: none;" data-path="${entry.Path}" data-type="d">${entry.Name}</li>`;
+            } else {
+                dom = `<li class="bi bi-file" style="list-style: none;"  data-path="${entry.Path}" data-type="f">${entry.Name}</li>`;
+            }
+            $('ul#filemanager').append(dom);
+        }
+    }
+
+    $(document).on('click', function (ev) {
+        ev.preventDefault();
+        let elem = ev.target;
+        let data = $(elem).data();
+        if (data.type === 'd') {
+            $.get('/files', { path: data.path }, function (s, g) {
+                EmptyList();
+            }).done(d => {
+                if (d?.records) {
+                    let records = d.records;
+                    RenderList(records);
+                }
+            }).fail(f => {
+                console.log({ f });
+            });
+        }
+    });
+
+    
+    $('button#home').on('click', function () {
+        // SEE/EXPECT QUERY CONTENT
+        $.get('/files', {id: 1, action: 'list'}, function (data) {
+            console.log({ data });
+            // clear all children from ul#filemanager
+            $('ul#filemanager').children().each((i, c) => {
+                $(c).empty();
+                $(c).remove();
+            });
+        }).done(data => {
+            console.log({ data });
+            // let up = data?.up;
+            let records = data?.records;
+            // // then 
+            RenderList(records);
+        }).fail(f => { console.log({ f }) });
+    });
+    // list item clicks
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var router = express.Router()
 var bodyParser = require('body-parser');
+var fs = require('fs/promises');
+var path = require('path');
+const _ = require('lodash');
+
+const ROOT = path.dirname(__dirname);
+
+console.log({ ROOT });
+
 
 // create application/json parser
 var jsonParser = bodyParser.json()
@@ -17,11 +25,77 @@ router.get('/', function (req, res, next) {
 router.get('/api1', jsonParser, urlencodedParser, function (req, res, next) {
     // $.get('/api', {id: 1, name: "titi"}, function(d) {}).done(d => {console.log(d)}).fail(f => {console.log(f)});
     console.log(`API1 GET query`, req.query);
-    console.log(`API1 GET body`, req.body);
+    // console.log(`API1 GET body`, req.body);
     // $.get('/api/1/titi', function(data) { console.log(data)}).done(d => {console.log(d)}).fail(f => {console.log(f)});
-    console.log(`API1 GET params`, req.params);
+    // console.log(`API1 GET params`, req.params);
     res.status(200).send('ok');
 });
+
+// list DIRECTORY contents route: QUERY shall contain path to display
+router.get('/files', jsonParser, urlencodedParser, async function (req, res, next) {
+    var currentDir = ROOT;
+    var query = req.query.path || '';
+    if (query) currentDir = path.join(ROOT, query);
+    let upDir;
+    if (currentDir !== ROOT) {
+        upDir = path.dirname(currentDir).replace(ROOT, '');
+    } else {
+        upDir = "";
+    }
+    console.log("browsing ", currentDir, upDir);
+    // if (currentDir === ROOT) {
+    //     res.status(201).send({ msg: 'already on the top' });
+    //     return;
+    // }
+    try {
+        let records = [];
+        let files = await fs.readdir(currentDir);
+        for (let idx in files) {
+            let f = files[idx];
+            let fpath = path.join(currentDir, f);
+            let st = await fs.stat(fpath);
+            // console.log({ f, currentDir, fpath, isDirectory: st.isDirectory() });
+            if (st.isDirectory()) {
+                if (!f.match(/^\./)) {
+                    records.push({ Name: f, IsDirectory: true, Up: upDir, Path: path.join(query, f), Mtime: st.mtime, Size: st.size });
+                }
+            } else {
+                if (!f.match(/^\./)) {
+                    let ext = path.extname(f);
+                    records.push({ Name: f, Ext: ext, IsDirectory: false, Up: upDir, Path: path.join(query, f), Mtime: st.mtime, Size: st.size });
+                }
+            }
+        }
+        // console.log({ files, records });
+        records = records.sort((a, b) => {
+            // Dir vs Dir
+            if (a.isDirectory === true && b.IsDirectory === true) {
+                // compare their names
+                if (a.Name < b.Name) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else if (a.IsDirectory === true && b.IsDirectory === false) {
+                return -1;
+            } else if (a.IsDirectory === false && b.IsDirectory === true) {
+                return 1;
+            } else if (a.IsDirectory === false && b.IsDirectory === false) {
+                if (a.Name < b.Name) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
+        res.status(202).send({msg: 'ok', code: 0, records, updir: upDir})
+    } catch (error) {
+        res.status(400).send({msg: error.message, code: error.code, records: []});
+    }
+    return;
+});
+
+
 
 // with params
 router.get('/api2/:id?/:name?', jsonParser, urlencodedParser, function (req, res, next) {
